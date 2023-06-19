1a3f3e003727b5aacffc37e4600918c397dd75e9
ZakiYoga <l200200229@student.ums.ac.id>
2023-06-19 16:58:19 +0700

    update post

--- src/component/data.jsx
+++ src/component/data.jsx
@@ -1,46 +1,55 @@
-import img from "../assets/img/support.jpg";
+import Post1 from "../assets/img/post/komoditas 1.jpg";
+import Post2 from "../assets/img/post/komoditas 2.jpg";
+import Post3 from "../assets/img/post/komoditas 3.jpg";
+import Post4 from "../assets/img/post/komoditas 4.jpg";
+import Post5 from "../assets/img/post/komoditas 5.jpg";
+import Post6 from "../assets/img/post/komoditas 6.jpg";
 
 const data = {
   cardData: [
     {
       id: 1,
-      img: img,
-      title: "Berita terkini tentang komoditas",
+      img: Post1,
+      title:
+        "Sepekan Jelang Lebaran, Harga Komoditas Pangan di Banda Aceh Merangkak Naik",
       desc: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
       button: "Read More",
     },
     {
       id: 2,
-      img: img,
-      title: "Berita terkini tentang komoditas",
+      img: Post2,
+      title:
+        "Ekspedisi Barang ke Malinau Terbatas, Komoditas Lokal Bisa jadi Peluang Tarik Penyedia Jasa",
       desc: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
       button: "Read More",
     },
     {
       id: 3,
-      img: img,
-      title: "Berita terkini tentang komoditas",
+      img: Post3,
+      title:
+        "Jelang Idul Adha Harga 4 Komoditas Pangan Naik, Ini Upaya Badan Pangan Nasional",
       desc: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
       button: "Read More",
     },
     {
       id: 4,
-      img: img,
-      title: "Berita terkini tentang komoditas",
+      img: Post4,
+      title: "Komoditas Ekspor Indonesia yang Menjadi Unggulan",
       desc: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
       button: "Read More",
     },
     {
       id: 5,
-      img: img,
-      title: "Berita terkini tentang komoditas",
+      img: Post5,
+      title: "Pengembangan Komoditas Kedelai Sleman Gandeng Perguruan Tinggi",
       desc: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
       button: "Read More",
     },
     {
       id: 6,
-      img: img,
-      title: "Berita terkini tentang komoditas",
+      img: Post6,
+      title:
+        "Indonesia Makin Dibanjiri Komoditas Pangan Impor, Mendag: Perlu dikendalikan",
       desc: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
       button: "Read More",
     },
